a13aa54045286e62d7252699784a2b523cbe20b8
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-09-14 23:41:09 -0700

    updating ./terraform-mutable-infra

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source = "./vendor/modules/vpc"
-  cidr_block = var.vpc_cidr_block
+  source = ""
+  vpc_cidr_block = var.vpc_cidr_block
   subnets = var.subnets
   env = var.env
 
